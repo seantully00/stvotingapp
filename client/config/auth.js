f986531a6533e47459b513cccc6ad57a19538cf8
Sean <seantully00@hotmail.com>
2016-12-14 23:52:12 +0000

    logging in with passport

--- client/config/auth.js
+++ client/config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
 	'twitterAuth': {
 		'consumerKey': process.env.TWITTER_CONSUMER_KEY,
     	'consumerSecret': process.env.TWITTER_CONSUMER_SECRET,
-    	'callbackURL': process.env.APP_URL + 'auth/twitter/callback'
+    	'callbackURL': 'https://stvotingapp.herokuapp.com/auth/twitter/callback'
 	}
 };
