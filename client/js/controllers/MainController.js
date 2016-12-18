90fbce26acee71c43638f88af597ef63e7d6b20c
Sean <seantully00@hotmail.com>
2016-12-18 02:25:56 +0000

    figuring our angular

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -1,6 +1,8 @@
 var sean="sean"
 
-myapp.controller('MainController', ['$scope', 'name', function($scope, name) {
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('MainController', ['$scope', 'name', function($scope, name) {
   name.success(function(sean) {
     $scope.name = sean;
   });
